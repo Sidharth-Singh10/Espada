6dd5c9ea6648b7b50565637f99cbd3e0eb1d6cf0
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-04-06 04:05:17 +0530

    commit4

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,32 @@ import Navbar from "./Components/Navbar";
 export default function Home() {
   return (
     <main >
-     <Navbar/>
-     <div className="h-screen w-screen bg-[#F9E8C9]">
-   
-     </div>
-    
+      <Navbar />
+      <div className="h-screen flex flex-col items-center bg-gradient-to-b from-[#FFFFFF] to-[#E5F0FF] ">
+
+        <h1 className=" relative top-[8%] text-5xl font-extrabold   ">
+          <span className="text-[#296aec]"> Transparent </span>
+          <span className="text-[#3b3f61]"> Donations, </span>
+          <span className="text-[#296aec]">  Tangible </span>
+          <span className="text-[#3b3f61]"> Results  </span>
+        </h1>
+
+        <div className=" relative font-medium text-2xl top-[12%]">
+          <h1>
+            Track donations pegged to gold,ensuring transparency
+          </h1>
+          <h1>
+            Stability, and impactful contributions to global causes
+          </h1>
+        </div>
+
+
+
+
+
+
+      </div>
+
     </main>
   );
 }
